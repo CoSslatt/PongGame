09fd198bd909c3e7b16b71bca67439976e343d4e
Narcikss <mdgamedeveloper@gmail.com>
2022-05-11 15:29:29 +0200

    game.js

--- logic/game.js
+++ logic/game.js
@@ -1,9 +1,21 @@
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
+const WALL_RECT_SIZE = new Array(40, 130);
+
 window.addEventListener("load", () => {
 	canvas.height = window.innerHeight - 250;
 	canvas.width = window.innerWidth - 650;
 
-	ctx.strokeRect(100, 100, 200, 200);
+	drawWalls();
 });
+
+function drawWalls() {
+	const wallPosY = (canvas.height - WALL_RECT_SIZE[1]) / 2;
+	const leftWall = ctx.fillRect(
+		0,
+		wallPosY,
+		WALL_RECT_SIZE[0],
+		WALL_RECT_SIZE[1]
+	);
+}
