80035e3ea0eaef5fd4d016c68502f8968241e712
Narcikss <mdgamedeveloper@gmail.com>
2022-05-13 00:34:34 +0200

    Add ball simple movement

--- logic/components/ball.js
+++ logic/components/ball.js
@@ -1,14 +1,33 @@
-class Ball {
-	constructor() {
-		this.x = 0;
-		this.y = 0;
-	}
-}
+const BALL_POS = new Array();
+let BALL_LOADED = false;
+
+let directionX = 2;
+let directionY = 2;
 
 export function drawBall(canvas, ctx) {
-	let circle = new Path2D();
-	circle.arc(100, 35, 15, 0, 2 * Math.PI);
+	ballStartPos(canvas);
+
+	ballMovement();
+
+	let ball = new Path2D();
+	ball.arc(BALL_POS[0], BALL_POS[1], 15, 0, 2 * Math.PI);
 
 	ctx.fillStyle = "blue";
-	ctx.fill(circle);
+	ctx.fill(ball);
 }
+
+function ballStartPos(canvas) {
+	if (BALL_LOADED) return;
+
+	BALL_POS[0] = canvas.width / 2;
+	BALL_POS[1] = canvas.height / 2;
+
+	BALL_LOADED = true;
+}
+
+function ballMovement() {
+	BALL_POS[0] += directionX;
+	BALL_POS[1] += directionY;
+}
+
+function checkCollisions() {}
